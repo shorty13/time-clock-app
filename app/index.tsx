0d116661af6af51730e148f34f00e60e31dce01a
Shorty <silvan.joerg@gmx.ch>
2025-12-03 19:21:46 +0100

    New blank Expo with Typescript + NativeWind setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import "../globals.css"
+
+export default function Index() {
+    return (
+        <View className='flex-1 items-center justify-center bg-black'>
+            <Text className='text-white'>Hello world!</Text>
+        </View>
+    )
+}
+
